8da7c3b21b11257b5864e50eb60b0662e2cd69b2
lukemcgovern <lmcgover@uvm.edu>
2024-04-22 18:20:27 -0400

    allow connection on port 3000, better FastAPI CRUD practices

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+# Connect FastAPI Application to MySQL database
 
 URL_DATABASE = 'mysql+pymysql://root:Dustinjohnson7@localhost:3306/Tarkov_Project'
 
