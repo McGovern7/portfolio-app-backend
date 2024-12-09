12a3f08947f9bf57a11d9bc90861aa4aa8ff4b59
lukemcgovern <lmcgover@uvm.edu>
2024-12-08 20:21:08 -0500

    getenv now working locally

--- auth.py
+++ auth.py
@@ -9,13 +9,17 @@ from models import User
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from jose import jwt, JWTError
+from dotenv import load_dotenv
 import os
+
+load_dotenv()  # Load the .env file
 
 router = APIRouter(
     prefix='/auth',
     tags=['auth']
 )
-
+# export SECRET_KEY='48541489F0A600CC6BE9AF391925AFC9F2AE317E2920CC91FB5EB287E6775AC5'
+# export ALGORITHM='HS256'
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRATION_TIME = 30
